84dd2da633f653167683a92b36d4c0ddbdab5360
Chi Nguyen <nguyen.thimai@northeastern.edu>
2021-02-10 00:01:21 -0500

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,17 @@
 // Code SimpleComponent Here
+import React, {useState, useEffect} from 'react';
+
+
+class SimpleComponent extends React.Component {
+  state = {mood: 'happy'};
+  render() {
+
+    return (
+      <div onClick={() => this.state.mood === "happy" ? this.setState({mood: "sad"}) : this.setState({mood: "happy"}) }>
+      {this.state.mood}
+      </div>
+      )
+    }
+}
+
+export default SimpleComponent;
